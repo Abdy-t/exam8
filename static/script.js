60166aa0eb80fe792b5e9fc904518af0a84b3251
abdygany_tabyldyev <tabyldyev34@gmail.com>
2020-04-25 13:45:08 +0600

    exam 8

---
+++ static/script.js
@@ -0,0 +1,40 @@
+'use strict';
+async function getCountry() {
+    let content = document.querySelector('[name=title]').value;
+    console.log(content);
+    if (content == undefined || content.trim().length == 1)
+        return;
+    let url = 'https://restcountries.eu/rest/v2/name/' + content;
+    let response = await fetch(url);
+    if (response.ok) {
+        console.log(response);
+        let countries = await response.json();
+        let aa = JSON.stringify(countries);
+        console.log("aaa"+aa);
+        console.log(countries);
+        for (let i = 0; i < countries.length; i++){
+            let c = {
+                name: countries[i].name,
+                capital: countries[i].capital,
+                region: countries[i].region,
+                currencies: countries[i].currencies[i].name,
+                flag: countries[i].flag
+
+            };
+            console.log(c);
+            document.getElementsByTagName('div')[0].append(createCountryElement(c));
+        }
+    } else alert("Such a country does not exist")
+}
+function createCountryElement(c) {
+    let div = document.createElement('div');
+    let content = '<p style="font-size:200%;">' + c.name + '</p>' +
+                  '<img src=' + c.flag + '>' +
+                  '<p>'+ "Capital - " + c.capital + '</p>' +
+                  '<p>'+ "Region - " + c.region + '</p>' +
+                  '<p>'+ "Currencies - " + c.currencies + '</p>' +
+                  '<a href="https://www.google.com/search?q='+ c.name +'" target="_blank">More</a>'+
+                  '<br>';
+    div.innerHTML = content;
+    return div;
+}
